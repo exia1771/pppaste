484a0b51f7729486bd1f39a550f24e143a521638
ExiaXu <exia1771@gmail.com>
2025-05-22 22:59:16 +0800

    移除文件

--- paste.js
+++
@@ -1,6 +0,0 @@
-(function () {
-  // 执行主函数
-  chrome.runtime.sendMessage({ action: "getEditableElements" }, (response) => {
-    pasteContentToEditableElements(response?.editableElements || []);
-  });
-})();
